a5ffd1c0bbb994ee0bf72f05db18cecc45c9add6
Vyacheslav Molodyakov <vrmolodyakov@mail.ru>
2023-01-11 20:18:20 +0300

    env

--- server/app/internal/config/config.go
+++ server/app/internal/config/config.go
@@ -1,18 +1,45 @@
 package config
 
 import (
+	"errors"
 	"log"
 	"os"
 	"path/filepath"
 	"sync"
+	"time"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
+const (
+	configPath = "\\configs\\config.yaml"
+)
+
+type Postgres struct {
+	User     string `env:"POSTGRES_USER"     env-required:""`
+	Password string `env:"POSTGRES_PASSWORD" env-required:""`
+	Database string `env:"POSTGRES_DB"       env-required:""`
+	URL      string `env:"POSTGRES_URL"       env-required:""`
+}
+
+type HTTP struct {
+	IP           string        `yaml:"ip"`
+	Port         int           `yaml:"port"`
+	ReadTimeout  time.Duration `yaml:"read-timeout"`
+	WriteTimeout time.Duration `yaml:"write-timeout"`
+	CORS         struct {
+		AllowedMethods     []string `yaml:"allowed_methods"`
+		AllowedOrigins     []string `yaml:"allowed_origins"`
+		AllowCredentials   bool     `yaml:"allow_credentials"`
+		AllowedHeaders     []string `yaml:"allowed_headers"`
+		OptionsPassthrough bool     `yaml:"options_passthrough"`
+		ExposedHeaders     []string `yaml:"exposed_headers"`
+		Debug              bool     `yaml:"debug"`
+	} `yaml:"cors"`
+}
+
 type Config struct {
-	Port   string `yaml : "port"`
-	Host   string `yaml : "host"`
-	LogLvl string `yaml : "loglvl"`
+	HTTP HTTP `yaml:"http"`
 }
 
 var instance *Config
@@ -20,11 +47,28 @@ var once sync.Once
 
 func GetConfig() *Config {
 	once.Do(func() {
-		path, _ := os.Getwd()
-		root := filepath.Dir(filepath.Dir(filepath.Dir(filepath.Dir(path))))
-		if err := cleanenv.ReadConfig(root+"\\configs\\config.yaml", instance); err != nil {
-			log.Fatal(err)
+		rootPath, _ := os.Getwd()
+		root := filepath.Dir(filepath.Dir(filepath.Dir(filepath.Dir(rootPath))))
+		instance = &Config{}
+		path := root + configPath
+		if exist, _ := Exists(path); exist {
+			if err := cleanenv.ReadConfig(path, instance); err != nil {
+				log.Fatal(err)
+			}
 		}
 	})
+	var P Postgres
+	cleanenv.ReadEnv(P)
 	return instance
+}
+
+func Exists(name string) (bool, error) {
+	_, err := os.Stat(name)
+	if err == nil {
+		return true, nil
+	}
+	if errors.Is(err, os.ErrNotExist) {
+		return false, nil
+	}
+	return false, err
 }
